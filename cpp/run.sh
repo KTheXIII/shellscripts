d62ed38d9e38680bae7d2408d0ed62c776d69bd5
mononerv <pratchaya.k.git@gmail.com>
2022-08-18 09:40:13 +0200

    cpp: ccache to run.sh

--- cpp/run.sh
+++ cpp/run.sh
@@ -3,23 +3,38 @@
 # Compile and run single C++ file
 set -e
 
+# Build
+ESCAPE_CODE='\033'
+RED='\033[0;31m'
+NC='\033[0m'      # No Color
+if [ $# -eq 0 ]; then
+    printf "${RED}No input file${NC} (っ- ‸ – ς)\n"
+    printf "\n"
+    printf "Usage: ${0} {filename}.cpp\n"
+    exit 1
+fi
+
 bin=bin
 obj=obj
-
-in=$1
-out="${in%.*}"
+mkdir -p $bin
+mkdir -p $obj
 
 cpp_version=-std=c++20
-warnings="-Wall -Wextra -Wpedantic -Werror -Wno-missing-field-initializers"
-input_file=$1
-target_dir=bin
-include_dir="-I."
-library=""
-compile_flags="${cpp_version} ${warnings} ${include_dir} ${library}"
+warnings='-Wall -Wextra -Wpedantic -Werror'
+includes=""   # Use $(pkg-config --cflags fmt)
+libraries=""  # Use $(pkg-config --libs fmt)
 
-# compile
-c++ $compile_flags $in -o $bin/$out
+input=$1
+out="${input%.*}"
+target=${bin}/${out}
 
-# run
-./bin/$out "${@:2}"
+printf "Building ${out} (*′☉.̫☉)..."
+
+ccache c++ $cpp_version $includes $libraries $warnings $input -o $target
+#c++ $cpp_version $includes $libraries $warnings $input -o $target
+
+printf ' Done! (^～^)\n'
+
+# Run
+./${bin}/${out} "${@:2}"
 
